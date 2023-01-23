2927b27af352db76934e32f0c5d76af64c942ef2
iermoliq <quentin.iermoli@gmail.com>
2023-01-23 10:35:59 +0100

    ajout back et dossiers

---
+++ back/src/main/java/im2agoracle/univgrenoblealpes/fr/gromed/GromedApplication.java
@@ -0,0 +1,13 @@
+package im2agoracle.univgrenoblealpes.fr.gromed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GromedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GromedApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/im2agoracle/univgrenoblealpes/fr/gromed/GromedApplicationTests.java
@@ -0,0 +1,13 @@
+package im2agoracle.univgrenoblealpes.fr.gromed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GromedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

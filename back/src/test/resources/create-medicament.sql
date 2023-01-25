6a1171d5f6d36ad19f5cb4418338f02c640f2d1b
m1miage-bonninal <bonnin.alice@gmail.com>
2023-01-25 16:54:56 +0100

    adapt column names in sql script

--- back/src/test/resources/create-medicament.sql
+++ back/src/test/resources/create-medicament.sql
@@ -1,4 +1,4 @@
-INSERT INTO Medicament (codeCIS, denomination_medicament, est_commercialise, est_en_surveillance_renforcee) 
+INSERT INTO Medicament (codecis, denominationmedicament, estcommercialise, estensurveillancerenforcee) 
 VALUES (123, 'medoc de test', false, false);
-INSERT INTO Presentation (id, agrement_collectivites, codecip13, codecip7, est_autorise,  prix, taux_remboursement, codecis)
+INSERT INTO Presentation (id, agrementcollectivites, codecip13, codecip7, estautorise,  prix, tauxremboursement, codecis)
 VALUES (1, true, 13, 7, true, 19.9, 70, 123);

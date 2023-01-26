5fd2d84ae9149bd0ee8dcd343880fab74dbdf667
m1miage-bonninal <bonnin.alice@gmail.com>
2023-01-26 16:07:02 +0100

    query get panier from etab

--- back/src/main/java/im2agoracle/univgrenoblealpes/fr/gromed/repository/CommandeRepository.java
+++ back/src/main/java/im2agoracle/univgrenoblealpes/fr/gromed/repository/CommandeRepository.java
@@ -1,6 +1,9 @@
 package im2agoracle.univgrenoblealpes.fr.gromed.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import im2agoracle.univgrenoblealpes.fr.gromed.entities.Commande;
@@ -8,4 +11,6 @@ import im2agoracle.univgrenoblealpes.fr.gromed.entities.Commande;
 @Repository
 public interface CommandeRepository extends JpaRepository<Commande, Integer>{
     
+    @Query(value="select c from commande c where c.idetablissement = ?1 and estpanier = 1", nativeQuery = true)
+    Optional<Commande> findPanierByIdEtab(int idEtab);
 }

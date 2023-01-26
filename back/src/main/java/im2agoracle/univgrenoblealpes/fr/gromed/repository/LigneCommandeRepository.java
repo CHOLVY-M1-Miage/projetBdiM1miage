4debc77395365682d46b5cc797ede86349259d07
m1miage-bonninal <bonnin.alice@gmail.com>
2023-01-26 10:46:07 +0100

    get detail commande

--- back/src/main/java/im2agoracle/univgrenoblealpes/fr/gromed/repository/LigneCommandeRepository.java
+++ back/src/main/java/im2agoracle/univgrenoblealpes/fr/gromed/repository/LigneCommandeRepository.java
@@ -1,5 +1,7 @@
 package im2agoracle.univgrenoblealpes.fr.gromed.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,5 @@ import im2agoracle.univgrenoblealpes.fr.gromed.keys.LigneCommandeKey;
 @Repository
 public interface LigneCommandeRepository extends JpaRepository<LigneCommande, LigneCommandeKey>{
     
+    List<LigneCommande> findByIdCommande(int idCommande);
 }
